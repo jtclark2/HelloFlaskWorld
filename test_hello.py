7b5da449773829138d94e0816f24e0d0bf0c632e
Trevor Clark <jtclark2@gmail.com>
2021-08-13 22:03:02 +0000

    Setup flask hello world scaffolding.

---
+++ test_hello.py
@@ -0,0 +1,12 @@
+from hello import hello, echo
+
+
+def test_hello():
+    assert hello() == "Hello World"
+
+
+# # This needs flask specific mocking, outside scope of this tutorial 
+# def test_echo():
+#     result = echo("Trevor")
+#     print("*" * 50 + result + "*" * 50)
+#     assert echo("Trevor") == "new-name: Trevor"
